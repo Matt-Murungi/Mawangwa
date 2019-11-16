576de5cf3217409b707ebfd39780cca72363b1dc
Matt-Murungi <murungima@gmail.com>
2019-11-16 17:24:13 +0300

    first commit

---
+++ mawangwa/urls.py
@@ -0,0 +1,10 @@
+from my_app import views
+from django.contrib import admin
+from django.urls import path, include
+
+
+urlpatterns = [
+
+    path('admin/', admin.site.urls),
+    path('', include('my_app.urls'))
+]
